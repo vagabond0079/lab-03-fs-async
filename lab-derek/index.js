d3fda091b32d4cd03d84f9b62463d3608ee0649a
derekcfarmer <derek.farmer@gmail.com>
2017-06-14 15:07:20 -0700

    have the index.js printing data, trying to get printFile to access correct file

--- lab-derek/index.js
+++ lab-derek/index.js
@@ -1,11 +1,32 @@
+'use strict';
+
 // TODO: create an `index.js` that calls your function with three paths from process.argv
 //   and then logs the contents of the three files to the screen
 //   TODO: (your console.log should be  in the index.js file not in your print-files function)
 
+const fs = require('fs');
+const pf = require('./lib/print-files.js');
+const index = module.exports = {};
 
+index.main = () => {
 
+  let paths = ['../data/test1.txt'];
+
+  let result = pf.printFiles(paths);
+  // fs.readFile(paths, (err, data) => {
+    //   if(err)
+    //     return console.error(err);
+    //   console.log(data.toString());
+    // });
+
+  console.log('I\'m reading, can you wait a millisecond?');
+  console.log(result);
+  return result;
+};
+
+index.main();
 
 
 // TODO:  Write tests that guarantee that the files are logged in order.
 // TODO:  You will also need to look up and implement async testing, making sure that you are not getting "false positives" (tests that are passing but shouldn't be).
-//   TODO:  hint read mocha docs    
+//   TODO:  hint read mocha docs
--- lab-derek/lib/print-files.js
+++ lab-derek/lib/print-files.js
@@ -1,10 +1,20 @@
 //  it should have a function signature (paths, callback) => undefined
 // TODO:  expect the callback function signature to be (err, data) => undefined
 
-const printFiles = module.exports = {};
+const fs = require('fs');
 
-printFiles = (paths, (err, data)) => undefined;
+const pf = module.exports = {};
+
+pf.printFiles = (paths) =>
+  fs.readFile(paths[0], (err, data) => {
+    if(err)
+      return console.error(err);
+    console.log(data.toString());
+  });
 
 // TODO:  read the three files in order
+
+
+
 // TODO:  on success pass an array of the contents of the files the data param of the callback `callback(null, data)`
 // TODO:  on failure pass and error in to the callback `callback(err)`

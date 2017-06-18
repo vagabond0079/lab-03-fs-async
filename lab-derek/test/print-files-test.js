f0b2f347fc911a4822141f66859bb65f8c23cb0f
derekcfarmer <derek.farmer@gmail.com>
2017-06-17 21:45:11 -0700

    used real pics

--- lab-derek/test/print-files-test.js
+++ lab-derek/test/print-files-test.js
@@ -8,32 +8,17 @@
 const pf = require('../lib/print-files.js');
 const expect = require('expect');
 
-// describe('pf.printFiles', function() {
-//   it("should return 'test1', 'test2', 'test3' in that order", function() {
-//     let results = [];
-//     let pfTest1 = pf.printFiles(['test1', 'test2', 'test3'], (err, data) => {
-//       beforeEach(function(done){
-//         results = data.toString();
-//         expect(results).toEqual(data);
-//         done();
-//       });
-//     return results;
-//   });
-//     expect(pfTest1).toEqual(['test1', 'test2', 'test3']);
-//   });
-// });
-
-describe('testing printFiles', () =>{
-  it('should resolve an array with ["one", "two", "three"]', (done) =>{
-    let paths = [
-      `${__dirname}/data/one.txt`,
-      `${__dirname}/data/one.txt`,
-      `${__dirname}/data/one.txt`,
-    ];
-    pf.printFiles(paths, (err, data) => {
-      if (err) return done(err);
-      expect(data).toEqual(['one', 'two', 'three']);
-      done();
-    });
+describe('pf.printFiles', function() {
+  it("should return 'test1', 'test2', 'test3' in that order", function() {
+    let results = [];
+    let pfTest1 = pf.printFiles(['test1', 'test2', 'test3'], (err, data) => {
+      beforeEach(function(done){
+        results = data.toString();
+        expect(results).toEqual(data);
+        done();
+      });
+    return results;
+  });
+    expect(pfTest1).toEqual(['test1', 'test2', 'test3']);
   });
 });
